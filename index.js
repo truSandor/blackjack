e21afa9f7ef4cd6be3fb6ef3bf8629ea02efd0f3
T.Sanyi <s.truczkai@gmail.com>
2023-07-23 15:58:16 +0200

    implement displaying sum

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ let firstCard = 10
 let secondCard = 11
 let hasBlackJack = false
 let isAlive = true
-let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
 
 let sum = firstCard + secondCard
 
-
 function startGame() {
     if (sum < 21) {
-        message = "do you wat to draw a card?"
+        messageEl.textContent = "Do you wat to draw a card?"
     } else if (sum === 21) {
-        message = "Blackjack"
+        messageEl.textContent = "You've got a Blackjack!"
         hasBlackJack = true
     } else {
-        message = "you went over 21!"
+        messageEl.textContent = "You went over 21!"
         isAlive = false
     }
 
-    console.log(message)
+    sumEl.textContent = "Sum: " + sum
 }
