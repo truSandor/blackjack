fc30e12bbd1a96fbb89dab2816dfa0d5e234dfc6
T.Sanyi <s.truczkai@gmail.com>
2023-07-23 19:35:08 +0200

    fix newCard to only allow drawing new card when the game is not over

--- index.js
+++ index.js
@@ -33,10 +33,12 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    renderGame()
+    if (isAlive && !hasBlackJack) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
 }
 
 function getRandomCard() {
